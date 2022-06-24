2e6a576f9e396522a63b140140a2a8b5effa3cb4
Luca Gaddini <90861515+lucagad@users.noreply.github.com>
2022-06-24 16:24:59 +0200

    {Laravel} Fixed: about / contact page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,5 +1,7 @@
 @extends('layout.main')
 
 @section('content')
-<h1>ABOUT</h1>
+<main class="container col-xxl-8 px-2 py-2 d-flex justify-content-center align-items-center">
+    <h1>About Us</h1>
+</main>
 @endsection
